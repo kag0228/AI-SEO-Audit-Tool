760f31e17d11649f899eb1a0f2de0edbf0e47b3f
Kevin Gibson <38744791+kag0228@users.noreply.github.com>
2026-05-13 22:28:31 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ai-seo-audit-tool/',
+  base: '/AI-SEO-Audit-Tool/',
   build: {
     outDir: 'dist',
     sourcemap: false
